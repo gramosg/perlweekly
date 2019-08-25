4325599afe25cf8773d6f9d546cfe64208b9303d
Guillermo Ramos <gramos@gramos.me>
2019-08-25 12:13:06 +0200

    [022#1] (WIP)

---
+++ 022/ch2.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/env perl
+#
+# Write a script to implement Lempel–Ziv–Welch (LZW) compression algorithm. The
+# script should have method to encode/decode algorithm. The wiki page explains
+# the compression algorithm very nicely.
+#
+# (https://en.wikipedia.org/wiki/Lempel%E2%80%93Ziv%E2%80%93Welch).
+################################################################################
+
+use strict;
+use warnings;
+
